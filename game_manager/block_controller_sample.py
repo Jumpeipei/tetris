5950345315b39eacb6d5e5cc900415f96c105e75
Jumpeipei <Softtank@outlook.jp>
2022-09-09 17:04:47 +0900

    update controller

--- game_manager/block_controller_sample.py
+++ game_manager/block_controller_sample.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import pprint
 import copy
 
+
 class Block_Controller(object):
 
     # init parameter
@@ -18,7 +19,7 @@ class Block_Controller(object):
     # GetNextMove is main function.
     # input
     #    nextMove : nextMove structure which is empty.
-    #    GameStatus : block/field/judge/debug information. 
+    #    GameStatus : block/field/judge/debug information.
     #                 in detail see the internal GameStatus data.
     # output
     #    nextMove : nextMove structure which includes next shape position and the other.
@@ -28,7 +29,7 @@ class Block_Controller(object):
 
         # print GameStatus
         print("=================================================>")
-        pprint.pprint(GameStatus, width = 61, compact = True)
+        pprint.pprint(GameStatus, width=61, compact=True)
 
         # get data from GameStatus
         # current shape info
@@ -50,10 +51,12 @@ class Block_Controller(object):
         # search with current block Shape
         for direction0 in CurrentShapeDirectionRange:
             # search with x range
-            x0Min, x0Max = self.getSearchXRange(self.CurrentShape_class, direction0)
+            x0Min, x0Max = self.getSearchXRange(
+                self.CurrentShape_class, direction0)
             for x0 in range(x0Min, x0Max):
                 # get board data, as if dropdown block
-                board = self.getBoard(self.board_backboard, self.CurrentShape_class, direction0, x0)
+                board = self.getBoard(
+                    self.board_backboard, self.CurrentShape_class, direction0, x0)
 
                 # evaluate board
                 EvalValue = self.calcEvaluationValueSample(board)
@@ -62,13 +65,13 @@ class Block_Controller(object):
                     strategy = (direction0, x0, 1, 1)
                     LatestEvalValue = EvalValue
 
-                ###test
-                ###for direction1 in NextShapeDirectionRange:
+                # test
+                # for direction1 in NextShapeDirectionRange:
                 ###  x1Min, x1Max = self.getSearchXRange(self.NextShape_class, direction1)
-                ###  for x1 in range(x1Min, x1Max):
+                # for x1 in range(x1Min, x1Max):
                 ###        board2 = self.getBoard(board, self.NextShape_class, direction1, x1)
                 ###        EvalValue = self.calcEvaluationValueSample(board2)
-                ###        if EvalValue > LatestEvalValue:
+                # if EvalValue > LatestEvalValue:
                 ###            strategy = (direction0, x0, 1, 1)
                 ###            LatestEvalValue = EvalValue
         # search best nextMove <--
@@ -86,7 +89,8 @@ class Block_Controller(object):
         #
         # get x range from shape direction.
         #
-        minX, maxX, _, _ = Shape_class.getBoundingOffsets(direction) # get shape x offsets[minX,maxX] as relative value.
+        # get shape x offsets[minX,maxX] as relative value.
+        minX, maxX, _, _ = Shape_class.getBoundingOffsets(direction)
         xMin = -1 * minX
         xMax = self.board_data_width - maxX
         return xMin, xMax
@@ -95,11 +99,12 @@ class Block_Controller(object):
         #
         # get coordinate array by given shape.
         #
-        coordArray = Shape_class.getCoords(direction, x, y) # get array from shape direction, x, y.
+        # get array from shape direction, x, y.
+        coordArray = Shape_class.getCoords(direction, x, y)
         return coordArray
 
     def getBoard(self, board_backboard, Shape_class, direction, x):
-        # 
+        #
         # get new board.
         #
         # copy backboard data to make new board.
@@ -109,10 +114,10 @@ class Block_Controller(object):
         return _board
 
     def dropDown(self, board, Shape_class, direction, x):
-        # 
+        #
         # internal function of getBoard.
         # -- drop down the shape on the board.
-        # 
+        #
         dy = self.board_data_height - 1
         coordArray = self.getShapeCoordArray(Shape_class, direction, x, 0)
         # update dy
@@ -145,25 +150,25 @@ class Block_Controller(object):
         height = self.board_data_height
 
         # evaluation paramters
-        ## lines to be removed
+        # lines to be removed
         fullLines = 0
-        ## number of holes or blocks in the line.
+        # number of holes or blocks in the line.
         nHoles, nIsolatedBlocks = 0, 0
-        ## absolute differencial value of MaxY
+        # absolute differencial value of MaxY
         absDy = 0
-        ## how blocks are accumlated
+        # how blocks are accumlated
         BlockMaxY = [0] * width
         holeCandidates = [0] * width
         holeConfirm = [0] * width
 
-        ### check board
+        # check board
         # each y line
         for y in range(height - 1, 0, -1):
             hasHole = False
             hasBlock = False
             # each x line
             for x in range(width):
-                ## check if hole or block..
+                # check if hole or block..
                 if board[y * self.board_data_width + x] == self.ShapeNone_index:
                     # hole
                     hasHole = True
@@ -173,7 +178,8 @@ class Block_Controller(object):
                     hasBlock = True
                     BlockMaxY[x] = height - y                # update blockMaxY
                     if holeCandidates[x] > 0:
-                        holeConfirm[x] += holeCandidates[x]  # update number of holes in target column..
+                        # update number of holes in target column..
+                        holeConfirm[x] += holeCandidates[x]
                         holeCandidates[x] = 0                # reset
                     if holeConfirm[x] > 0:
                         nIsolatedBlocks += 1                 # update number of isolated blocks
@@ -192,7 +198,7 @@ class Block_Controller(object):
         for x in holeConfirm:
             nHoles += abs(x)
 
-        ### absolute differencial value of MaxY
+        # absolute differencial value of MaxY
         BlockMaxDy = []
         for i in range(len(BlockMaxY) - 1):
             val = BlockMaxY[i] - BlockMaxY[i+1]
@@ -200,34 +206,33 @@ class Block_Controller(object):
         for x in BlockMaxDy:
             absDy += abs(x)
 
-        #### maxDy
+        # maxDy
         #maxDy = max(BlockMaxY) - min(BlockMaxY)
-        #### maxHeight
+        # maxHeight
         #maxHeight = max(BlockMaxY) - fullLines
 
-        ## statistical data
-        #### stdY
-        #if len(BlockMaxY) <= 0:
+        # statistical data
+        # stdY
+        # if len(BlockMaxY) <= 0:
         #    stdY = 0
-        #else:
+        # else:
         #    stdY = math.sqrt(sum([y ** 2 for y in BlockMaxY]) / len(BlockMaxY) - (sum(BlockMaxY) / len(BlockMaxY)) ** 2)
-        #### stdDY
-        #if len(BlockMaxDy) <= 0:
+        # stdDY
+        # if len(BlockMaxDy) <= 0:
         #    stdDY = 0
-        #else:
+        # else:
         #    stdDY = math.sqrt(sum([y ** 2 for y in BlockMaxDy]) / len(BlockMaxDy) - (sum(BlockMaxDy) / len(BlockMaxDy)) ** 2)
-
 
         # calc Evaluation Value
         score = 0
-        score = score + fullLines * 10.0           # try to delete line 
+        score = score + fullLines * 10.0           # try to delete line
         score = score - nHoles * 1.0               # try not to make hole
         score = score - nIsolatedBlocks * 1.0      # try not to make isolated block
         score = score - absDy * 1.0                # try to put block smoothly
-        #score = score - maxDy * 0.3                # maxDy
-        #score = score - maxHeight * 5              # maxHeight
-        #score = score - stdY * 1.0                 # statistical data
-        #score = score - stdDY * 0.01               # statistical data
+        # score = score - maxDy * 0.3                # maxDy
+        # score = score - maxHeight * 5              # maxHeight
+        # score = score - stdY * 1.0                 # statistical data
+        # score = score - stdDY * 0.01               # statistical data
 
         # print(score, fullLines, nHoles, nIsolatedBlocks, maxHeight, stdY, stdDY, absDy, BlockMaxY)
         return score
